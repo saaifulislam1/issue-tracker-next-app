d81a66514078c20b22c7832de5b679da61889984
saiful <azadahmedofficial@gmail.com>
2024-02-13 01:53:02 +0600

    working api

--- prisma/migrations/20240212193837_/migration.sql
+++ prisma/migrations/20240212193837_/migration.sql
@@ -9,3 +9,14 @@ CREATE TABLE `Issue` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `blog` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(225) NOT NULL,
+    `description` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
