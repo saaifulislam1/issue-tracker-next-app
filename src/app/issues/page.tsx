6050b513a3fedf6f6c9581d11eb94ac2eb4cb079
saiful <azadahmedofficial@gmail.com>
2024-02-14 01:21:18 +0600

    radix ui added

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuePage = () => {
-  return <div>Issue page</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuePage;
