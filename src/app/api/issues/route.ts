0d0f43abf2c17863dbc698555c5d3abcfcc1dde8
saiful <azadahmedofficial@gmail.com>
2024-02-20 23:57:07 +0600

    implement client side validation

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,13 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import z from "zod";
 import { PrismaClient } from "@prisma/client";
-// this is where we are hitting: http://localhost:3000/api/issues
-// we are hitting into-> api/issues,
-// this is schema for data we will recieving
-const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required").max(255),
-  description: z.string().min(1, "description is required"),
-});
+import { createIssueSchema } from "../../validationSchema";
 // creating instance of Prisma
 const prisma = new PrismaClient();
 
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,20 +1,41 @@
 "use client";
-import { Button, Callout, CalloutRoot, TextField } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  CalloutRoot,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+// this is the new issue form page
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
+
+// we will replace this with z
+type IssueForm = z.infer<typeof createIssueSchema>;
+
 const NewIssuePage = () => {
   const [error, setError] = useState("");
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   return (
     <div className="max-w-xl">
       {error && (
@@ -34,8 +55,14 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root>
+          {/* here , the register("title")-> is grabing the title column of the table  */}
           <TextField.Input {...register("title")} placeholder="Title" />
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -43,6 +70,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
 
         <Button className="cursor-pointer">Submit New Issue</Button>
       </form>
