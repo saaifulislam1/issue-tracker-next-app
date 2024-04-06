a08b352fdfe37d79cef7da422367eef936998926
saiful <azadahmedofficial@gmail.com>
2024-04-06 21:48:45 +0600

    Added component

---
+++ src/app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,24 @@
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card } from "@radix-ui/themes";
+import React from "react";
+import Markdown from "react-markdown";
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return;
+  <>
+    <Heading color="violet" className="text-2xl">
+      {issue.title}
+    </Heading>{" "}
+    <Flex direction={"row"} gap={"2"} className="mb-2">
+      <i className="bg-slate-300 px-2 py-[1px] text-center text-[10px]">
+        {issue.status}
+      </i>
+      <p className="italic text-[10px]">{issue.createdAt.toDateString()}</p>
+    </Flex>
+    <Card className="mt-3 prose">
+      <Markdown>{issue.description}</Markdown>
+    </Card>
+  </>;
+};
+
+export default IssueDetails;
