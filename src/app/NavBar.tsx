55259e944e14ac85d7be3a552eeb7c9cce3d5a95
saiful <azadahmedofficial@gmail.com>
2024-04-11 19:21:25 +0600

    added Provider for userSession

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,12 +1,16 @@
 "use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { IoBugSharp } from "react-icons/io5";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
   // gives us currentPath
   const currentPath = usePathname();
+  // const { status, data: session } = useSession();
+  // console.log(session?.user?.email);
 
   const links = [
     { label: "DashBoard", href: "/" },
@@ -19,15 +23,16 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={`${
-              link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
-            } hover:text-zinc-800 transition-color`}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={`${
+                link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+              } hover:text-zinc-800 transition-color`}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
