3561a0f6e385b3a9c1ee20204c0090c92d2dd427
saiful <azadahmedofficial@gmail.com>
2024-04-12 16:16:56 +0600

    implementing middleware func, securing app,

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import EditIssueButton from "./EditIssueButton";
-import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
+import EditIssueButton from "../_components/EditIssueButton";
+import IssueDetails from "../_components/IssueDetails";
+import DeleteIssueButton from "../_components/DeleteIssueButton";
+import { getServerSession } from "next-auth";
 interface Props {
   params: { id: string };
 }
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession();
   //   if (typeof params.id !== "number") notFound();
   const prisma = new PrismaClient();
   const issue = await prisma.issue.findUnique({
@@ -20,10 +22,12 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="md:col-span-4 w-full">
         <IssueDetails issue={issue} />
       </Box>
-      <Flex direction={"column"} gap={"4"} className="md:colspa">
-        <EditIssueButton IssueID={issue.id} />
-        <DeleteIssueButton IssueID={issue.id} />
-      </Flex>
+      {session && (
+        <Flex direction={"column"} gap={"4"} className="md:colspa">
+          <EditIssueButton IssueID={issue.id} />
+          <DeleteIssueButton IssueID={issue.id} />
+        </Flex>
+      )}
     </Grid>
   );
 };
