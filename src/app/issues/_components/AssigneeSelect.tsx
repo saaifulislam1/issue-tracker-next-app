2d40c27ead1d735f59302b1b737250616dd317d9
saiful <azadahmedofficial@gmail.com>
2024-04-12 19:06:23 +0600

    /api/user/ created and integrated

--- src/app/issues/_components/AssigneeSelect.tsx
+++ src/app/issues/_components/AssigneeSelect.tsx
@@ -1,20 +1,35 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
-    // <div>
     <Select.Root>
       <Select.Trigger placeholder="Assign..."></Select.Trigger>
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="1">Saif</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.email}
+            </Select.Item>
+          ))}
+
+          {/* <Select.Item value="{user.id}">sd</Select.Item> */}
         </Select.Group>
       </Select.Content>
     </Select.Root>
-    // </div>
   );
 };
 
